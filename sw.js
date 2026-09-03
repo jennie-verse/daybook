9fdc96bb4dad951a238bacec98a56c1cdc7df81a
jennie-verse <labs.flour.68@icloud.com>
2026-09-03 14:35:33 -0500

    fix: collapse journal header to two rows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '2026.09.03-headercontrols1';
+const VERSION = '2026.09.03-headercontrols2';
 const CACHE = `daybook-${VERSION}`;
 const SHELL = [
   './', './index.html', './assets/app.css',
