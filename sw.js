f817537a1eb0b6e4c0b817a70bc87ab5bed6170f
jennie-verse <labs.flour.68@icloud.com>
2026-09-03 15:09:56 -0500

    Compact mobile footer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '2026.09.03-headercontrols2';
+const VERSION = '2026.09.03-controlcompact1';
 const CACHE = `daybook-${VERSION}`;
 const SHELL = [
   './', './index.html', './assets/app.css',
